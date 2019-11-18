4e05591920d40e6f362068ca88e76f39a4e8006e
Kate Sava <ekaterinasava@gmail.com>
2019-11-18 17:44:55 +0100

    UPD 'App' component / start 'revert' method

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ArticleList from './ArticleList';
 import articles from '../fixtures';
 import 'bulma/css/bulma.css';
 
-function App() {
-  return (
-    <div className="articles">
-      <section className="hero is-dark">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Article List</h1>
-            <h2 className="subtitle">Awesome list of articles</h2>
+class App extends Component {
+
+  state = {
+    reverted: false
+  }
+
+  render() {
+    return (
+      <div className="articles">
+        <section className="hero is-dark">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Article List</h1>
+              <h2 className="subtitle">Awesome list of articles</h2>
+            </div>
           </div>
+        </section>
+  
+        <div className="container">
+          <nav class="navbar">
+            <div class="navbar-end">
+              <button className="button is-link is-outlined" onClick = {this.revert}>Revert</button>
+            </div>
+          </nav>
         </div>
-      </section>
-      <div className="container">
-        <ArticleList articles = {articles} />
+  
+        <div className="container">
+          <ArticleList articles = {articles} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  revert = () => this.setState({
+    reverted: !this.state.reverted
+  })
 }
 
 export default App;
