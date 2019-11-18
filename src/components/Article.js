1fe07763351044fa932b1d89a269600a17e969de
Kate Sava <ekaterinasava@gmail.com>
2019-11-18 13:39:00 +0100

    UPD 'Article': ADD view toggler

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Article(props) {
-  const {article} = props
+class Article extends Component {
 
-  const body = <section>{article.text}</section>
+  state = {
+    isOpen: true
+  }
 
-  return (
-    <div className="hello">
-      <h2>{article.title}</h2>
-      {body}
-      <h3>creation date: {(new Date (article.date)).toDateString()}</h3>
-    </div>
-  )
+  render() {
+    const {article} = this.props
+
+    const body = this.state.isOpen && <section>{article.text}</section>
+
+    return (
+      <div className="hello">
+        <h2>
+          {article.title}
+          <button onClick={this.handleClick}>{this.state.isOpen ? 'close' : 'open'}</button>
+        </h2>
+        {body}
+        <h3>creation date: {(new Date (article.date)).toDateString()}</h3>
+      </div>
+    )
+  }
+
+  handleClick = () => {
+    console.log('---', 'clicked')
+
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
 }
 
 export default Article
